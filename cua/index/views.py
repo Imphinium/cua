d2df6dadc2008ad583ceec7ea1a006facaee05f2
rauldesarrolla <xceptionalbro@gmail.com>
2019-03-26 23:07:47 +0100

    Indice añadido

--- cua/index/views.py
+++ cua/index/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
+
+
+def index(request):
+	context = {}
+	return render(request, 'index/index.html', context)
