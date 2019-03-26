a01443f88d347aecaad4d9620ba8704f6b4c6978
rauldesarrolla <xceptionalbro@gmail.com>
2019-03-27 00:31:07 +0100

    Creado modelo items

--- cua/mod/models.py
+++ cua/mod/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+import secrets
+import string
 
 # Create your models here.
+
+class IDClass():
+	def newID():
+		choice_set = string.ascii_uppercase + string.digits
+		newID = ""
+		while newID == "":
+			for i in range(24):
+				chosen = secrets.choice(choice_set)
+				newID += chosen
+		return newID
+
+class Item(models.Model):
+	id = models.CharField(primary_key=True, default=IDClass.newID, max_length=12, unique=True, editable=False)
+	name = models.CharField(max_length=128)
+	description = models.CharField(max_length=512)
+
+
+	def __str__ (self):
+		return f'{self.name}'
