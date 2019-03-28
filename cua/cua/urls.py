5e6a3bb55c90b8503aac723eec8abe390bf61669
Frenzoid <frenzoid@protonmail.com>
2019-03-28 21:23:51 +0100

    Update urls.py

--- cua/cua/urls.py
+++ cua/cua/urls.py
@@ -19,21 +19,8 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-import os
-
-"""
- Frenzoid: We should add a suburl manipulator, in case this project gets deployed on a suburl, for example
-           www.mydomain.com/cua/.
-           The suburl can be changed changing the env var or using a dotenv file.
-"""
-
-suburl = '';
-# If the env var is not empty, and if its value isn't "default", append the suburl.
-if os.environ['SUBURL'] and os.environ['SUBURL'] != 'default':
-    suburl = os.environ['SUBURL']
-
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('index.urls')),
-] + static(suburl + settings.FILES_URL, document_root=settings.FILES_ROOT)
+    path('^$', include('index.urls')),
+    path('^admin/', admin.site.urls),
+] + static(settings.FILES_URL, document_root=settings.FILES_ROOT)
