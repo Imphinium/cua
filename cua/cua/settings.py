06f43264af68e60732b665aacba392d8636681b8
Frenzoid <frenzoid@protonmail.com>
2019-03-28 21:15:07 +0100

    Update settings.py

--- cua/cua/settings.py
+++ cua/cua/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
-FILES_URL = '/files/'
+STATIC_URL = 'static/'
+FILES_URL = 'files/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 FILES_ROOT = os.path.join(BASE_DIR, 'files')
