38e5b9793666991b11947c37c05077e233b4db0f
Frenzoid <frenzoid@protonmail.com>
2019-03-29 00:07:45 +0100

    Update urls.py

--- cua/index/urls.py
+++ cua/index/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('results/', views.results, name='results'),
