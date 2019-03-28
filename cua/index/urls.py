bd1acf4f7826482915e387a517995e85f8367890
Frenzoid <frenzoid@protonmail.com>
2019-03-29 00:06:47 +0100

    Update urls.py

--- cua/index/urls.py
+++ cua/index/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('',views.index, name='index'),
+    path('', views.index, name='index'),
     path('results/', views.results, name='results'),
     path('details/<str:item_id>/', views.details, name='details'),
     path('upload/', views.upload, name='upload'),
