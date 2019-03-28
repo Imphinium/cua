31b37f775951854903d81d6f8e139e11fbdff149
Frenzoid <frenzoid@protonmail.com>
2019-03-28 15:50:54 +0100

    Update urls.py

--- cua/cua/urls.py
+++ cua/cua/urls.py
@@ -19,8 +19,15 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+import os
+
+suburl = '';
+
+if os.environ['SUBURL']:
+    suburl = os.environ['SUBURL']
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('index.urls')),
+    path(suburl + 'admin/', admin.site.urls),
+    path(suburl, include('index.urls')),
 ] + static(settings.FILES_URL, document_root=settings.FILES_ROOT)
