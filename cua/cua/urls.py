91f94df3d326dd7295584b889450468c20b2b3b4
Frenzoid <frenzoid@protonmail.com>
2019-03-29 00:07:23 +0100

    Update urls.py

--- cua/cua/urls.py
+++ cua/cua/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('', include('index.urls')),
     path('admin/', admin.site.urls),
+    path('', include('index.urls')),
 ] + static(settings.FILES_URL, document_root=settings.FILES_ROOT)
