55263f17aa0f48175ff73e15ac460caf49632e0c
Frenzoid <frenzoid@protonmail.com>
2019-03-28 22:40:38 +0100

    Update urls.py

--- cua/index/urls.py
+++ cua/index/urls.py
@@ -1,16 +1,10 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-import os
-
-suburl = '';
-# If the env var is not empty, and if its value isn't "default", append the suburl.
-if os.environ['SUBURL'] and os.environ['SUBURL'] != 'default':
-    suburl = os.environ['SUBURL']
 
 urlpatterns = [
-    path(suburl, views.index, name='index'),
-    path(suburl + '/results/', views.results, name='results'),
-    path(suburl + '/details/<str:item_id>/', views.details, name='details'),
-    path(suburl + '/upload/', views.upload, name='upload'),
+    path('',views.index, name='index'),
+    path('results/', views.results, name='results'),
+    path('details/<str:item_id>/', views.details, name='details'),
+    path('upload/', views.upload, name='upload'),
 ]
