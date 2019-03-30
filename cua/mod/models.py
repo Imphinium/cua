9d0547096f2b3cc9ba49ab94ee579864708bb046
rauldesarrolla <xceptionalbro@gmail.com>
2019-03-30 20:36:06 +0100

    Modelos actualizados

--- cua/mod/models.py
+++ cua/mod/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 import secrets
 import string
 
@@ -14,11 +16,43 @@ class IDClass():
 				newID += chosen
 		return newID
 
+class Tag(models.Model):
+	name = models.CharField(primary_key=True, max_length=128, editable=True)
+	description = models.CharField(max_length=512)
+	creation_date = models.DateTimeField(default=timezone.now)
+	creator = models.ForeignKey(User, default=None, null=True, on_delete=models.SET_NULL)
+
+	def __str__(self):
+		return f'{self.name}'
+
 class Item(models.Model):
-	id = models.CharField(primary_key=True, default=IDClass.newID, max_length=12, unique=True, editable=False)
+	id = models.CharField(primary_key=True, default=IDClass.newID, max_length=12, editable=False)
 	name = models.CharField(max_length=128)
 	description = models.CharField(max_length=512)
 	file = models.FileField(upload_to="files/{0}".format(IDClass.newID()), null=True)
+	creation_date = models.DateTimeField(default=timezone.now)
+	creator = models.ForeignKey(User, default=None, null=True, on_delete=models.SET_DEFAULT)
+
+
+	def __str__ (self):
+		return f'{self.name}'
+
+class Tag_Request(models.Model):
+	name = models.CharField(primary_key=True, max_length=128, editable=True)
+	description = models.CharField(max_length=512)
+	creation_date = models.DateTimeField(default=timezone.now)
+	creator = models.ForeignKey(User, default=None, null=True, on_delete=models.SET_NULL)
+
+	def __str__(self):
+		return f'{self.name}'
+
+class Item_Request(models.Model):
+	id = models.CharField(primary_key=True, default=IDClass.newID, max_length=12, editable=False)
+	name = models.CharField(max_length=128)
+	description = models.CharField(max_length=512)
+	file = models.FileField(upload_to="files/{0}".format(IDClass.newID()), null=True)
+	creation_date = models.DateTimeField(default=timezone.now)
+	creator = models.ForeignKey(User, default=None, null=True, on_delete=models.SET_DEFAULT)
 
 
 	def __str__ (self):
