fb9a4e6a30079791504994ab0856a3a3fd9a5ca4
rauldesarrolla <xceptionalbro@gmail.com>
2019-03-31 18:27:59 +0200

    Añadidas las vistas y templades "tags" e "input"

--- cua/mod/urls.py
+++ cua/mod/urls.py
@@ -6,4 +6,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.index, name='index_mod'),
     path('logout/', views.logout_view, name='logout_mod'),
+    path('input/', views.input_view, name='input_mod'),
+    path('tags/', views.tags_view, name='tags_mod'),
 ]
--- cua/mod/views.py
+++ cua/mod/views.py
@@ -3,6 +3,7 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth import logout
 from django.urls import reverse
 from django.http import HttpResponseRedirect
+from .models import *
 
 # Create your views here.
 
@@ -15,3 +16,12 @@ def logout_view(request):
 	if request.user.is_authenticated:
 		logout(request)
 	return HttpResponseRedirect(reverse('index'))
+
+@staff_member_required
+def input_view(request):
+	contest = {'input':Item_Request.objects.all()}
+	return render(request, 'mod/input.html')
+
+@staff_member_required
+def tags_view(request):
+	return render(request, 'mod/tags.html')
